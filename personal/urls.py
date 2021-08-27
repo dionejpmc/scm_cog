2b2c9190066018b8f4409e42fc76944464289f84
dionejpmc <dionejpmc@gmail.com>
2021-08-27 16:32:56 -0300

    Commit de atualização

--- personal/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.urls import path, include
-
-urlpatterns = [
-    path('personal/changepassword', views.ChangePassword, name="changepassword"),
-]
